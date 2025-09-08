01827af8b0ae59de949ca99c40f1f81adf3be818
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-09-08 13:58:16 +0200

    add logout functionallity, use persist middleware for user's store

--- src/features/user/profile.tsx
+++ src/features/user/profile.tsx
@@ -1,19 +1,21 @@
 "use client"
-import { useUserStore } from "./store"
+import { userStore, useUserStore } from "./store"
 
 export function UserProfile() {
   const user = useUserStore(state => state.user)
-  const setName = useUserStore(state => state.setName)
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     const formData = new FormData(e.currentTarget)
     const username = String(formData.get("name"))
     if (!username) return
-    setName(username)
+    userStore().setName(username)
   }
   return (
     <div className="mt-5 flex items-center justify-center w-full h-full">
       {user ?
-        <h1 className="text-6xl font-black">Hello, {user.name}</h1>
+        <section className="flex flex-col items-center gap-3">
+          <h1 className="text-6xl font-black">Hello, {user.name}</h1>
+          <button className="bg-red-500 rounded-lg p-3" onClick={userStore().logout}>Logout</button>
+        </section>
         : (
           <form onSubmit={handleSubmit} className="flex gap-5">
             <input name="name" className="bg-white rounded-lg text-black px-3" placeholder="Enter your name" />
--- src/features/user/store.ts
+++ src/features/user/store.ts
@@ -1,14 +1,23 @@
 import { create } from "zustand";
 import { User } from "./types";
+import { persist } from "zustand/middleware";
 
 type UserState = {
   user: User | null;
   setName: (value: string) => void;
+  logout: () => void;
 }
 
-export const useUserStore = create<UserState>((set) => ({
+
+export const useUserStore = create<UserState>()(persist((set) => ({
   user: null,
   setName: (value: string) => {
     set((prev) => ({ ...prev, user: { name: value } }))
+  },
+  logout: () => {
+    set({ user: null })
+    localStorage.removeItem("userStore")
   }
-}))
+}), { name: "userStore" }))
+
+export const userStore = () => useUserStore.getState()
